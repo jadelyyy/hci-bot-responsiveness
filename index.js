25b2e4a94561a271acd9f19a00538cf5cecd384a
Michelle J. Wang <mjwang@andrew.cmu.edu>
2020-05-03 18:27:20 -0700

    testing

--- index.js
+++ index.js
@@ -2,23 +2,38 @@ const { Octokit } = require("@octokit/rest");
 const core = require("@actions/core");
 const github = require("@actions/github");
 
-function run () {
-    const userToken  = core.getInput('repo-token');
-    const repoName = core.getInput('repo-name');
-    const repoOwner = 'jadelyyy';
-
-    var octokit = new github.GitHub(userToken);
-
-    const {status, data: issues} = octokit.issues.listForRepo({
-        repoOwner,
-        repoName,
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
+};
 
-    if (status !== 200) {
-        throw new Error(`Received unexpected API status code ${status}`);
-    }
+function run () {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const userToken  = core.getInput('repo-token');
+            const repoName = core.getInput('repo-name');
+            const repoOwner = 'jadelyyy';
 
-    console.log('num issues: ' + issues.length);
+            var octokit = new github.GitHub(userToken);
+
+            const {status, data: issues} = yield octokit.issues.listForRepo({
+                repoOwner,
+                repoName,
+            });
+
+            // if (status !== 200) {
+            //     throw new Error(`Received unexpected API status code ${status}`);
+            // }
+
+            console.log('num issues: ' + issues.length);
+        } catch(err) {
+            console.log(err);
+        }
+    });
 }
 
 run();
