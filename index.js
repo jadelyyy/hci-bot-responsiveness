8135ecbc72261929a0e30906d30208c7a66ad461
Michelle J. Wang <mjwang@andrew.cmu.edu>
2020-05-03 18:17:57 -0700

    oops

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const github = require("@actions/github");
 function run () {
     const userToken  = core.getInput('repo-token');
     const repoName = core.getInput('repo-name');
+    const repoOwner = 'jadelyyy';
 
     var octokit = new github.GitHub(userToken);
 
     const {data: issues} = octokit.issues.listForRepo({
-        owner,
-        repo,
+        repoOwner,
+        repoName,
     });
 
     console.log('num issues: ' + issues.length);
