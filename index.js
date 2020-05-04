23339452f4614d68deeb2dc054417985783d5e5f
Michelle J. Wang <mjwang@andrew.cmu.edu>
2020-05-04 14:15:36 -0700

    oops

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 
-function getIssueComments(octokit, repoOwner, repoName, issueNumber) {
+function getFirstResponseTime(octokit, repoOwner, repoName, issueNumber) {
     return __awaiter(this, void 0, void 0, function* () {
         console.log('getting comments...\n');
         // const userToken  = core.getInput('repo-token');
