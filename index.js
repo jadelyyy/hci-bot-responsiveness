c289720d25dbe6dbdede7abc271103962d9bc53e
Michelle J. Wang <mjwang@andrew.cmu.edu>
2020-06-09 00:20:24 -0700

    huh

--- index.js
+++ index.js
@@ -568,7 +568,7 @@ function run () {
             // get month duration
             var currDate = new Date();
             var currMonth = currDate.getMonth();
-            var baseMonth = currMonth - 1;
+            var baseMonth = currMonth - 2;
             baseMonth = currMonth;
             var baseYear = currDate.getYear();
             if(baseMonth < 0) {
